db2b862db5c9ba5d1d071e8efda5ee205a3f4771
Yulia_Tarasenko <tyler000dennen@gmail.com>
2021-02-18 01:39:44 +0200

    Added navigation

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+function General() {
+  return (
+    <View style={styles.container}>
+      <Text>Юлія Тарасенко</Text>
+      <Text>Група ІО-82</Text>
+      <Text>3К ІО-8222</Text>
+    </View>
+  );
+}
+
+function SecondPage() {
+  return (
+    <View style={styles.container}>
+      <Text>Second page</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+              screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'General') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Second page') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="General" component={General} />
+        <Tab.Screen name="Second page" component={SecondPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
