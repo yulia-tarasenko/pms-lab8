50643b16f8e58505575d6cfea6bf539470dd5599
Yulia_Tarasenko <tyler000dennen@gmail.com>
2021-04-22 17:27:02 +0300

    Done lab2

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Switch, Dimensions  } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {LineChart, PieChart} from "react-native-chart-kit";
 
 function General() {
   return (
@@ -16,9 +17,96 @@ function General() {
 }
 
 function SecondPage() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  let fnX = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
+            1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2,
+            2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3,
+            3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4];
+  let fnY = fnX.map(x => Math.log(x));
+  
+  const data = [
+    {
+      percent: 10,
+      color: "yellow"
+    }, 
+    {
+      percent: 20,
+      color: "green"
+    },
+    {
+      percent: 25,
+      color: "blue"
+    },
+    {
+      percent: 5,
+      color: "red"
+    },
+    {
+      percent: 40,
+      color: "turquoise"
+    }
+  ];
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2,
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false
+  };
+  let main = <PieChart
+              data={data}
+              width={Dimensions.get("window").width}
+              height={250}
+              chartConfig={chartConfig}
+              accessor={"percent"}
+              backgroundColor={"transparent"}
+              paddingLeft={"15"}
+              center={[0, 0]}
+              absolute
+            />;
+
+  if (isEnabled) {
+    main = <LineChart data={{
+      datasets: [
+        {
+          data: fnY
+        }
+      ]
+    }}
+    width={Dimensions.get("window").width}
+    height={220}
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}/>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Second page</Text>
+      <Switch onValueChange={toggleSwitch} value={isEnabled}/>
+      {main}
     </View>
   );
 }
@@ -38,10 +126,9 @@ export default function App() {
                 ? 'ios-information-circle'
                 : 'ios-information-circle-outline';
             } else if (route.name === 'Second page') {
-              iconName = focused ? 'ios-list-box' : 'ios-list';
+              iconName = 'ios-list';
             }
 
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
